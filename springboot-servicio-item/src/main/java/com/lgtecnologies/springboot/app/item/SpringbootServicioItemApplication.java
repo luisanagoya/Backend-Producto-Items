9f4676386e9ee1279abcbfd27cd72d605fc8eb18
luisanagoyac@gmail.com <micorreo@gmail.com>
2020-10-07 16:57:00 +0200

    configurando los microservicios producto-item como clientes del servidor de Eureka

--- springboot-servicio-item/src/main/java/com/lgtecnologies/springboot/app/item/SpringbootServicioItemApplication.java
+++ springboot-servicio-item/src/main/java/com/lgtecnologies/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,11 +2,12 @@ package com.lgtecnologies.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-@RibbonClient(name="servicio-productos")
+
 @EnableFeignClients
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringbootServicioItemApplication {
 
 	public static void main(String[] args) {
