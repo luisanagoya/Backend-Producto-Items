dac5f7bbea7ceeb1393636202361d1f6e0176492
luisanagoya <luisanagoyac@gmail.com>
2020-10-08 16:57:46 +0200

    implementacion servidor de configuracion y creando perfiles dev y prod.

--- springboot-servicio-item/src/main/java/com/lgtecnologies/springboot/app/item/controller/ItemController.java
+++ springboot-servicio-item/src/main/java/com/lgtecnologies/springboot/app/item/controller/ItemController.java
@@ -1,9 +1,18 @@
 package com.lgtecnologies.springboot.app.item.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,38 +22,61 @@ import com.lgtecnologies.springboot.app.item.models.Producto;
 import com.lgtecnologies.springboot.app.item.models.service.IItemService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+@RefreshScope
 @RestController
 @Qualifier("itemServiceFeing")
 public class ItemController {
-	
+
+	private static Logger log = LoggerFactory.getLogger(ItemController.class);
+
+	@Autowired
+	private Environment env;
+
 	@Autowired
 	private IItemService itemService;
-	
+
+	@Value("${configuracion.texto}")
+	private String texto;
+
 	@GetMapping("/listar")
-	public List<Item> listar(){
+	public List<Item> listar() {
 		return itemService.findAll();
-		
+
 	}
-	
-	@HystrixCommand(fallbackMethod="metodoAlternativo")
+
+	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
-	public Item ver(@PathVariable Long id, @PathVariable Integer cantidad ) {
-		
+	public Item ver(@PathVariable Long id, @PathVariable Integer cantidad) {
+
 		return itemService.findById(id, cantidad);
 	}
-	
+
 	public Item metodoAlternativo(Long id, Integer cantidad) {
 		Item item = new Item();
-		Producto producto= new Producto();
-		
+		Producto producto = new Producto();
+
 		item.setCantidad(cantidad);
 		producto.setId(id);
 		producto.setNombre("camara Sony");
 		producto.setPrecio(500.00);
 		item.setProducto(producto);
 		return item;
-		
+
 	}
-	
+
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+		log.info(texto);
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
+
+			json.put("autor.nombre", env.getProperty("configuracion.autor.name"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+	}
 
 }
